3b3c5643f2405e28fd9917abe0b796a21ad4a753
Lauren-pople <57408708+Lauren-pople@users.noreply.github.com>
2019-11-05 18:03:22 +0000

    Update index.js

--- Data/index.js
+++ Data/index.js
@@ -2,29 +2,29 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-app.listen(3000, () => console.log("listening at port 3000"));
+app.listen(3000, () => console.log("listening at port 3000")); // calls terminal host 
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
-const database = new Datastore('database.db');
+const database = new Datastore('database.db'); // stores the data collected in data base 
 database.loadDatabase();
 
-app.post('/api', (request, response) => {
-    const data = request.body;
-    const timestamp = Date.now();
-    data.timestamp = timestamp;
-    database.insert(data);
-    response.json(data);
+app.post('/api', (request, response) => { // api
+    const data = request.body; 
+    const timestamp = Date.now(); //timestamp is today's date
+    data.timestamp = timestamp; // creates timestamp
+    database.insert(data); // data from db is inserted into api
+    response.json(data); 
 });
 
 
-app.get('/api', (request, response) => {
-    database.find({}).sort({timestamp: 1}).limit(999).exec(function (err,docs){
+app.get('/api', (request, response) => { // displays api
+    database.find({}).sort({timestamp: 1}).limit(999).exec(function (err,docs){ // finds and then sorts the data by timestamp going up in the value 1
         if (err) {
             console.log(err);
             response.end();
             return;
         }
-        response.json(docs);
+        response.json(docs); 
     });
 });
